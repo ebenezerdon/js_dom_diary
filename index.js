ee4f004e4a422d6055461b355c03101c032e6d94
Ebenezer Don <ebenezerdonu@gmail.com>
2020-06-16 19:27:03 +0100

    add user input to entries section on submit

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+const entryTextbox = document.querySelector('.entry-textbox');
+const entriesSection = document.querySelector('#entries');
+
 function addEntryToDom(event) {
   event.preventDefault();
+
+  const entryDiv = document.createElement('div');
+  entryDiv.className = 'single-entry';
+  entryDiv.innerText = entryTextbox.value;
+  entriesSection.appendChild(entryDiv);
 }
 
 const entryForm = document.querySelector('#entryForm');
